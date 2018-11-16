f27c61d99d91adfb29a8c7d4024c3469a29b26e7
Irina Usoltceva <irina.710@mai;.ru>
2018-11-16 17:44:02 +0300

    поле готово

--- rikochet/rikochet.js
+++ rikochet/rikochet.js
@@ -19,16 +19,25 @@ function init() {
 
     //Timer
 }*/
+var WIDTH = 640;
+var HEIGHT = 640;
+
 var BRICK_W = 100;
 var BRICK_H = 30;
 var BRICK_X0 = 100;
 var BRICK_Y0 = 100;
+
+var CAR_W = 100;
+var CAR_H = 30;
+
+var BALL_RADIUS = 20;
 
 function init() {
     console.info("page loaded");
     var stage = new createjs.Stage("game");
 
     //сами объекты
+    //кирпич для постройки
     var brick = new createjs.Shape;
     brick.graphics
         .beginFill("black")
@@ -38,6 +47,7 @@ function init() {
     brick.regX = BRICK_W / 2;
     brick.regY = BRICK_H / 2;
 
+    //кирпичи на уровне
     var bricks = [
         [
             {x: BRICK_X0, y: BRICK_Y0},
@@ -46,12 +56,11 @@ function init() {
             {x: BRICK_X0 + 3 * BRICK_W, y: BRICK_Y0},
             {x: BRICK_X0 + 4 * BRICK_W, y: BRICK_Y0}
         ], [
-            {x: BRICK_X0 + 2 * BRICK_W, y: BRICK_Y0 + BRICK_Y0}
+            {x: BRICK_X0 + 2 * BRICK_W, y: BRICK_Y0 + BRICK_H}
         ]
     ];
-    brick.x = 100;
-    brick.y = 100;
 
+    //контейнер с кирпичами
     var bricksContainer = new createjs.Container();
     for (var i = 0; i < bricks.length; i++) {
         for (var j = 0; j < bricks[i].length; j++) {
@@ -62,12 +71,33 @@ function init() {
         }
     }
 
-    brick2 = brick.clone(true);
-    brick2.x = 100;
-    brick2.y = 132;
+    //машина
+    var car = brick.clone(true);
+    car.x = WIDTH / 2;
+    car.y = HEIGHT - CAR_H / 2;
 
-    stage.addChild(bricksContainer);
+    //мяч
+    var ball = new createjs.Shape;
+    ball.graphics
+        .beginFill("Black")
+        .drawCircle(WIDTH / 2, HEIGHT - CAR_H - BALL_RADIUS, BALL_RADIUS)
+        .beginFill("#50afe4")
+        .drawCircle(WIDTH / 2, HEIGHT - CAR_H - BALL_RADIUS, BALL_RADIUS - 1);
 
+    stage.addChild(bricksContainer, car, ball);
+
+    /*
+    function ball_tick(e) {
+        console.log('asdf');
+        var ball = e.target; //e - информация о событии, e.target - для кого вызвано
+        ball.x += ball.dx;
+        ball.y += ball.dy;
+
+        if (ball.x > WIDTH - R || ball.x < R)
+            ball.dx *= -1;
+        if (ball.y > HEIGHT - R || ball.y < R)
+            ball.dy *= -1;
+    }*/
 
     createjs.Ticker.addEventListener('tick', stage);
     createjs.Ticker.framerate = 60; //60 в секунду
