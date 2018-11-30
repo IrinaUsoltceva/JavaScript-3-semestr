51093a7a0ef6d745a67a4b6e3b11ba70e9619a11
Irina Usoltceva <irina.710@mai;.ru>
2018-11-30 19:38:13 +0300

    рикошет, отбивается!

--- ricochet_circle/rikochet.js
+++ ricochet_circle/rikochet.js
@@ -70,8 +70,9 @@ function init() {
     var ball = new createjs.Shape;
     ball.x = car.x;
     ball.y = HEIGHT - CAR_H - BALL_RADIUS;
-    ball.dx = 2;
-    ball.dy = -2;
+    ball.dx = 1;
+    ball.dy = -1;
+    ball.speed = 5;
     ball.r = BALL_RADIUS;
     ball.graphics
         .beginFill("Black")
@@ -92,10 +93,35 @@ function init() {
 
 //функции для работы
 
+    function collide(x0, y0, x, y, dx, dy) {
+        var vx = x - x0;
+        var vy = y - y0;
+        // var mod_v = Math.sqrt(vx * vx + vy * vy);
+        var mod_v2 = vx * vx + vy * vy;
+        console.log(vx, vy, mod_v2);
+        // ball.dx -= 2 * (vx / mod_v) *
+        //     ((vx * ball.dx + vy * ball.dy) / mod_v);
+        // ball.dy -= 2 * (vy / mod_v) *
+        //     ((vx * ball.dx + vy * ball.dy) / mod_v);
+
+        console.log('^', dx, dy);
+
+        var dot_prod = vx * dx + vy * dy;
+        dx -= 2 * (vx / mod_v2) * dot_prod;
+        dy -= 2 * (vy / mod_v2) * dot_prod;
+
+        console.log(2 * (vx / mod_v2) * (dot_prod), 2 * (vy / mod_v2) * (dot_prod));
+        console.log(dx, dy);
+
+        return [dx, dy];
+    }
+
+    console.log('c', collide(1, 3, 0, 0, 4, 7));
+
     function ball_tick(e) {
         var ball = e.target;
-        ball.x += ball.dx;
-        ball.y += ball.dy;
+        ball.x += ball.dx * ball.speed;
+        ball.y += ball.dy * ball.speed;
 
         //стенки
         if (ball.x > WIDTH - BALL_RADIUS || ball.x < BALL_RADIUS)
@@ -114,8 +140,9 @@ function init() {
             stage.addEventListener("stagemousedown", function () {
                 ball.x = car.x;
                 ball.y = HEIGHT - CAR_H - BALL_RADIUS;
-                ball.dx = 5;
-                ball.dy = -5;
+                ball.dx = 1;
+                ball.dy = -1;
+                ball.speed = 5;
                 stage.removeChild(text);
                 stage.addChild(ball);
                 stage_condition = 1;
@@ -126,9 +153,28 @@ function init() {
             ball.x <= car.x + CAR_W / 2 &&
             ball.x >= car.x - CAR_W / 2)
             ball.dy *= -1;
-        }
 
         //брики
+        for (var i = 0; i < bricks.length; i++) {
+            for (var j = 0; j < bricks[i].length; j++) {
+                //иными словами, если сумма радиусов больше, чем расстояние между центрами
+                //значит, мячи столкнулись
+                if ((BRICK_R + BALL_RADIUS) * (BRICK_R + BALL_RADIUS) >=
+                    (bricks[i][j].x - ball.x) * (bricks[i][j].x - ball.x) +
+                    (bricks[i][j].y - ball.y) * (bricks[i][j].y - ball.y)
+                ) {
+                    var d = collide(bricks[i][j].x, bricks[i][j].y, ball.x, ball.y, ball.dx, ball.dy);
+
+                    ball.dx = d[0];
+                    ball.dy = d[1];
+
+                    console.log("boom", ball.dx * ball.dx + ball.dy * ball.dy);
+
+                }
+            }
+        }
+    }
+
 
 
     stage.addEventListener('stagemousemove', function (e) {
